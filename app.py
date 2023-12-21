858954f1c84f717c6cf936e21d1096eda3f076d2
krunal2401 <109871648+krunal2401@users.noreply.github.com>
2023-12-21 13:55:53 +0530

    Update app.py
    added home page.

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<h1>Hello, The First CICD Jenkins has begun... It was an awsom experience. </h1>"
+
+@app.route("/home")
+def hello_world():
+    return "<h1>this is Home Page/</h1>"
